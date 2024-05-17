6da3d1ff5a45a34a9de968011d0dae5447b3129a
silvertxd <silvertxd@gmail.com>
2024-05-17 19:15:48 +0200

    change routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout.tsx'
 import { Airdrop } from './pages/Airdrop.tsx'
-import { Earn } from './pages/Earn.tsx'
 import { Exchange } from './pages/Exchange.tsx'
-import { Friends } from './pages/Friends.tsx'
-import { Mine } from './pages/Mine.tsx'
 export const router = createBrowserRouter([
     {
         path: '/',
@@ -14,18 +11,7 @@ export const router = createBrowserRouter([
                 path: '/',
                 element: <Exchange />,
             },
-            {
-                path: '/mine',
-                element: <Mine />,
-            },
-            {
-                path: '/friends',
-                element: <Friends />,
-            },
-            {
-                path: '/earn',
-                element: <Earn />,
-            },
+           
             {
                 path: '/airdrop',
                 element: <Airdrop />,
