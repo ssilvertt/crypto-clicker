16ac8f3ada5504d8d413b093f3a8cfa2eae53ff6
silvertxd <silvertxd@gmail.com>
2024-05-31 03:50:19 +0200

    Update user-store.ts

--- src/store/user-store.ts
+++ src/store/user-store.ts
@@ -8,6 +8,18 @@ interface InitInfo {
 }
 
 export const useUserData = create<InitInfo>(() => ({
-    initInfo: {},
+    initInfo: {
+        query_id: '',
+        user: {
+            id: 0,
+            first_name: '',
+            last_name: '',
+            username: '',
+            language_code: '',
+            allows_write_to_pm: false,
+        },
+        auth_date: 0,
+        hash: '',
+    },
     setInitInfo: (initData: InitData) => ({ initData }),
 }))
