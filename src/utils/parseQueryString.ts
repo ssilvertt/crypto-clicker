9a2b0d13b78c3834899214d5eac4203c566ca2bf
silvertxd <silvertxd@gmail.com>
2024-05-31 03:48:07 +0200

    fix parse func

--- src/utils/parseQueryString.ts
+++ src/utils/parseQueryString.ts
@@ -1,8 +1,21 @@
-export const parseQueryString = (str: string) => {
-    const urlParams = new URLSearchParams(str);
-    const obj = Object.fromEntries(urlParams);
-    
-    obj.user = JSON.parse(obj.user);
-    
-    return obj;
-};
+import type { InitData, User } from '../shared/types/user-data.ts';
+
+export function parseQueryString(queryString: string): InitData {
+    const params = new URLSearchParams(queryString);
+
+    const user: User = JSON.parse(params.get('user') || '{}');
+
+    return {
+        query_id: params.get('query_id') ?? '',
+        user: {
+            id: user.id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            username: user.username || '',
+            language_code: user.language_code,
+            allows_write_to_pm: user.allows_write_to_pm,
+        },
+        auth_date: parseInt(params.get('auth_date') || '0', 10),
+        hash: params.get('hash') ?? '',
+    };
+}
