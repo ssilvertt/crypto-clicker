de9529b75aff605cd0dc1e190791923a508cd818
silvertxd <silvertxd@gmail.com>
2024-06-03 18:29:23 +0200

    Create BottomButton.tsx

---
+++ src/components/BottomButton.tsx
@@ -0,0 +1,7 @@
+export function BottomButton({ text }: { text: string }) {
+    return (
+        <button className="bg-boost text-white font-bold py-2 px-4 rounded-[20px] text-button font-jakarta tracking-stats">
+            {text}
+        </button>
+    );
+}
