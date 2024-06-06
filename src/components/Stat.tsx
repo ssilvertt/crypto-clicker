cd2357fbfc021374fd8f807350346b8d3e696a20
silvertxd <silvertxd@gmail.com>
2024-06-06 18:03:13 +0200

    rename icon

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -6,7 +6,7 @@ interface StatProps {
 export function Stat({ text, value }: StatProps) {
     return (
         <div className="flex flex-col justify-center items-center">
-            <img className="h-6 w-6 text-muted" src="/coin.svg" alt="coin" />
+            <img className="h-6 w-6 text-muted" src="/coinIcon.svg" alt="coin" />
             <div className="tracking-stats text-stats text-muted">{text}</div>
             <p className="tracking-stats text-statsnum text-white font-bold">{value}</p>
         </div>
