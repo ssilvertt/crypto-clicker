e7b872785a9d9932d4e383da278b8ca031e10c8b
silvertxd <silvertxd@gmail.com>
2024-06-06 18:12:55 +0200

    saving clicks

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -1,48 +1,49 @@
+import { useCloudStorage } from '@vkruglikov/react-telegram-web-app';
 import { motion } from 'framer-motion';
 import { TouchEvent, useEffect, useState } from 'react';
 import { useClickerStore } from '../store/clicker-store.ts';
-import {useCloudStorage} from '@vkruglikov/react-telegram-web-app';
 
-export function PlayButton(){
-    const { increment, setClicks } = useClickerStore();
+export function PlayButton() {
+    const { increment, setClicks, count } = useClickerStore();
     const [clickCount, setClickCount] = useState(0);
     const { getItem, setItem } = useCloudStorage();
+
+    useEffect(() => {
+        const storedClicks = getItem('clicks');
+        if (storedClicks !== null) {
+            setClicks(Number(storedClicks));
+        }
+        
+        return () => {
+            setItem('clicks', count.toString());
+        };
+    }, [count, getItem, setClicks, setItem]);
+
     const handleTouchStart = (event: TouchEvent) => {
         if (event.touches.length === 1) {
             setClickCount((prevCount) => prevCount + 1);
         }
     };
-    
+
     const handleTouchEnd = () => {
         increment(clickCount);
         setClickCount(0);
     };
-    
-    useEffect(() => {
-        const clicks = getItem('clicks');
-        if (isNaN(Number(clicks))) {
-            setClicks(Number(clicks));
-        }
-        
-        return () => {
-            setItem('clicks', String(clickCount));
-        }
-    }, []);
-    
-return (
-    <motion.div
-        className=" mx-auto rounded-full relative mt-4"
-        onTouchStart={handleTouchStart}
-        onTouchEnd={handleTouchEnd}
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.9 }}
-        transition={{ type: 'spring', stiffness: 400, damping: 17 }}
-    >
-        <img
-            src={'/buttons.svg'}
-            alt={'button'}
-            className="w-[168px] h-[160px]"
-        />
-    </motion.div>
-)
+
+    return (
+        <motion.div
+            className=" mx-auto rounded-full relative mt-4"
+            onTouchStart={handleTouchStart}
+            onTouchEnd={handleTouchEnd}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+        >
+            <img
+                src={'/buttons.svg'}
+                alt={'button'}
+                className="w-[168px] h-[160px]"
+            />
+        </motion.div>
+    );
 }
