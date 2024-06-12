d3061ee164e1314e2c541443279ff56ecf427f3e
silvertxd <silvertxd@gmail.com>
2024-06-12 16:33:32 +0200

    change icon size

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -11,9 +11,9 @@ interface NavLinkProps {
 export function NavLink({ icon, text, path, isActive }: NavLinkProps) {
     return (
         <Link to={path}>
-            <div className="flex flex-col items-start justify-start w-[70px] h-[34px]">
+            <div className="flex flex-col items-center justify-start  w-[70px] h-[34px]">
                 <div
-                    className={`mx-auto ${isActive ? 'text-active' : 'text-light'}`}
+                    className={`${isActive ? 'text-active' : 'text-light'}`}
                 >
                     {icon}
                 </div>
