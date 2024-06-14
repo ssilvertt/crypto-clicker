486b48d1ae2ea8cd84b0c7cfc73b97a43c8e72b9
silvertxd <silvertxd@gmail.com>
2024-06-14 23:00:12 +0200

    add friends page

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -1,16 +1,70 @@
-
+import copy from '../copy.svg'
 
 export function Friends() {
-    
-    
     return (
-        
-        
-        
-        <>
-            <div className="">
-                friends
+        <div className="">
+            <div className="flex flex-col gap-2 items-center relative w-full">
+                <h1 className="text-center font-bold leading-10 text-[40px] text-light mt-8">
+                    Invite your friends
+                </h1>
+                <p className="text-center leading-6 text-base text-light">
+                    Your friend and you will receive bonuses
+                </p>
             </div>
-        </>
+            <div className="flex flex-col gap-2 items-center relative w-full mt-4">
+                <div className="rounded-3xl px-4 py-3 flex gap-4 items-center relative w-full bg-[#4508df]/[0.32]">
+                    <img src="/userIcon.svg" alt="user" />
+                    <div className="flex flex-col items-start flex-1 relative w-full">
+                        <p className="font-bold leading-6 text-base text-light">
+                            Invite a friend
+                        </p>
+                        <div className="flex gap-1 justify-center items-center relative">
+                            <img src="/coin.svg" alt="coin" />
+                            <p className="text-center leading-6 text-base text-light">
+                                + 5000 coins
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="rounded-3xl px-4 py-3 flex gap-4 items-center relative w-full bg-[#feb700]">
+                    <img src="/yellowUserIcon.svg" alt="user" />
+                    <div className="flex flex-col items-start flex-1 relative w-full">
+                        <p className="font-bold leading-6 text-base text-[#533a00]">
+                            Invite a 3 friends
+                        </p>
+                        <div className="flex gap-1 justify-center items-center relative">
+                            <img src="/coin.svg" alt="coin" />
+                            <p className="text-center leading-6 text-base text-[#533a00]">
+                                + 25000 coins
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-4 items-center flex-1 relative w-full h-[300px] mt-4">
+                <div className="flex gap-8 justify-between items-center relative w-full">
+                    <p className="leading-6 text-base text-light">
+                        Your friends list
+                    </p>
+                    <img src="/refresh.svg" alt="coin" />
+                </div>
+                <div className="rounded-2xl p-6 flex flex-col justify-between items-start flex-1 relative w-full h-full bg-[#4508df]/[0.32]">
+                    <h1 className="font-bold leading-10 text-[40px] text-light">
+                        You don't have any invited friends
+                    </h1>
+                    <div className="flex gap-[11px] items-start relative w-full ">
+                        <div className="rounded-[100px] px-6 py-3 flex gap-2 justify-center items-center flex-1 relative w-full bg-active">
+                            <p className="text-center leading-6 text-base text-[#130e24]">
+                                Invite Friends Now
+                            </p>
+                        </div>
+                        <div className="rounded-[100px] flex py-3 justify-center items-center  w-12 bg-active">
+                            <img src={copy} alt='copy' className='w-6 h-6'/>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
     );
 }
