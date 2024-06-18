7516397e92e6638f4fc7d18ae55e12cda0df93f4
silvertxd <silvertxd@gmail.com>
2024-06-18 03:06:23 +0200

    add scrolling elems

--- src/pages/DailyQ.tsx
+++ src/pages/DailyQ.tsx
@@ -15,17 +15,47 @@ export function DailyQ() {
                 Daily reward
             </p>
             <div className="flex flex-col gap-y-1 w-full">
-                <RewardCard title='Daily Reward' icon='/userIcon.svg' coinAmount={5000} />
+                <RewardCard
+                    title="Daily Reward"
+                    icon="/userIcon.svg"
+                    coinAmount={5000}
+                />
             </div>
             <p className="w-full text-base leading-normal text-light">
                 Special tasks
             </p>
-            <div className="flex flex-col gap-y-2 w-full">
-                <RewardCard title='Subscribe to the channel with project news' icon='/telegramIcon.svg' coinAmount={10000} />
-                <RewardCard title='Join our chat on Telegram' icon='/messageIcon.svg' coinAmount={10000} />
-                <RewardCard title='Subscribe to us on Twitter' icon='/twitterIcon.svg' coinAmount={15000} />
-                
+            <div className="flex flex-col gap-y-2 w-full relative">
+                <div className="overflow-y-auto h-72 flex flex-col gap-y-2 no-scrollbar">
+                    <RewardCard
+                        title="Subscribe to the channel with project news"
+                        icon="/telegramIcon.svg"
+                        coinAmount={10000}
+                    />
+                    <RewardCard
+                        title="Join our chat on Telegram"
+                        icon="/messageIcon.svg"
+                        coinAmount={10000}
+                    />
+                    <RewardCard
+                        title="Subscribe to us on Twitter"
+                        icon="/twitterIcon.svg"
+                        coinAmount={15000}
+                    />
+                    <RewardCard
+                        title="Join our chat on Telegram"
+                        icon="/messageIcon.svg"
+                        coinAmount={10000}
+                    />
+                    <RewardCard
+                        title="Subscribe to us on Twitter"
+                        icon="/twitterIcon.svg"
+                        coinAmount={15000}
+                    />
+                </div>
+                <div
+                    className="absolute inset-0 bg-gradient-to-b from-transparent from-60% to-black pointer-events-none"></div>
             </div>
+        
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,5 +27,20 @@ export default {
             }
         },
     },
-    plugins: [],
+    plugins: [
+        function({ addUtilities }) {
+            addUtilities({
+                '.overlay-container::after': {
+                    content: '""',
+                    position: 'absolute',
+                    top: 'calc(100% - 3rem)', // Adjust this value to position the overlay correctly
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    background: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black overlay
+                    pointerEvents: 'none',
+                },
+            }, ['responsive', 'hover']);
+        }
+    ],
 };
